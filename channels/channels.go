05081c9141d167a150e9bdb8c2fc456553d0d607
frenchtoasters <tfrench@linode.com>
2022-08-29 11:54:11 -0400

    Updating channel package

--- channels/channels.go
+++ channels/channels.go
@@ -1,46 +1,44 @@
-// package channel
+package main
 
-// import (
-// 	"context"
-// 	"time"
+import (
+	"context"
+	"time"
 
-// 	"k8s.io/klog/v2"
-// )
+	"k8s.io/klog/v2"
+)
 
-// type WalkieTalkie struct {
-// 	Content string
-// }
+type WalkieTalkie struct {
+	Content string
+}
 
-// var voices = make(chan WalkieTalkie, 10)
+var voices = make(chan WalkieTalkie, 10)
 
-// func (m *WalkieTalkie) Broadcast() {
-// 	ctx := context.Background()
-// 	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
-// 	defer cancel()
+func (m *WalkieTalkie) Broadcast() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
+	defer cancel()
 
-// 	klog.Infof("talkie: %s", m.Content)
-// }
+	klog.Infof("talkie: %s", m.Content)
+}
 
-// func walkieTalkie(q <-chan WalkieTalkie) {
-// 	for {
-// 		select {
-// 		case talkie := <-q:
-// 			talkie.Broadcast()
-// 		}
-// 	}
-// }
+func walkieTalkie(q <-chan WalkieTalkie) {
+	for {
+		select {
+		case talkie := <-q:
+			talkie.Broadcast()
+		}
+	}
+}
 
-// func message(talkie string) {
-// 	manager := &WalkieTalkie{
-// 		Content: talkie,
-// 	}
-// 	voices <- *manager
-// }
+func message(talkie string) {
+	manager := &WalkieTalkie{
+		Content: talkie,
+	}
+	voices <- *manager
+}
 
-// func main() {
-// 	go walkieTalkie(voices)
+func main() {
+	go walkieTalkie(voices)
 
-// 	message("10-4")
-
-// 	// time.Sleep(5 * time.Second)
-// }
+	message("10-4")
+}
