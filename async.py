0035f9fe7f438d5148a7c772694d1e2f39c83f26
frenchtoasters <tfrench@linode.com>
2022-10-31 19:39:41 -0400

    Adding python and deamonset

---
+++ async.py
@@ -0,0 +1,24 @@
+from asyncio import Future
+import asyncio
+
+
+async def plan(dinner: Future):
+    print('Planning my dinner...')
+    await asyncio.sleep(1)
+    dinner.set_result('Beef')
+
+
+def pickmeal() -> Future:
+    dinner = Future()
+    asyncio.create_task(plan(dinner))
+    return dinner
+
+
+async def main():
+    my_dinner = pickmeal()
+    result = await my_dinner
+
+    print(result)
+
+
+asyncio.run(main())
